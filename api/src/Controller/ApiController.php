cc9fd7b57555df03e400b9bd9d020334814b3c9f
Yao Jean-Claude <jeandev84@yahoo.com>
2019-12-04 15:57:31 +0300

    Creation d'un formulaire de recherche

--- api/src/Controller/ApiController.php
+++ api/src/Controller/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -20,26 +21,52 @@ class ApiController extends AbstractController
      */
     public function listeRegion(SerializerInterface $serializer)
     {
-        /*
-         on recupere toutes les regions depuis le lien d'une api externe : https://geo.api.gouv.fr/regions
-         qui est au format json
-        */
+        // on recupere les regions
         $mesRegionJson = file_get_contents('https://geo.api.gouv.fr/regions');
-        $mesRegions = $serializer->deserialize($mesRegionJson, 'App\Entity\Region[]', 'json');
+
+        // on recupere une collection de Region [ Array ]
+        $mesRegionsArray = $serializer->deserialize($mesRegionJson, 'App\Entity\Region[]', 'json');
 
         return $this->render('api/index.html.twig', [
-            'mesRegions' => $mesRegions
+            'mesRegions' => $mesRegionsArray
         ]);
     }
 
-
-    /*
-     * @Route("/api", name="api")
-    public function index()
+    /**
+     * @Route("/listeDepsParRegions", name="listeDepsParRegions")
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function listeDepsParRegions(Request $request, SerializerInterface $serializer)
     {
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
+        // on recupere la region selectionnee dans le formulaire
+        $codeRegion = $request->query->get('region');
+
+        // on recupere les regions
+        $mesRegionJson = file_get_contents('https://geo.api.gouv.fr/regions');
+        $mesRegionsArray = $serializer->deserialize($mesRegionJson, 'App\Entity\Region[]', 'json');
+
+        // on recupere la liste des departements
+        if($codeRegion == null || $codeRegion == "Toutes")
+        {
+            // on fait une recherche integrale de mon departement
+            $mesDepsJson = file_get_contents('https://geo.api.gouv.fr/departements');
+
+        } else {
+
+            // on fait la recherche qui correspond a la recheche de departement dont selectionne
+            $mesDepsJson = file_get_contents('https://geo.api.gouv.fr/regions/'. $codeRegion .'departements');
+        }
+
+        // decodage du format json en tableau
+        // on precise qu'on passe au decode() un format de type json
+        $mesDepsTab = $serializer->decode($mesDepsJson, 'json');
+
+        return $this->render('api/listDepsParRegion.html.twig', [
+            'mesRegions' => $mesRegionsArray,
+            'mesDeps' => $mesDepsTab
         ]);
     }
-    */
+
 }
