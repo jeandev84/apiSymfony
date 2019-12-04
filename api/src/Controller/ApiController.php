ca55ae72031640bd35acb373f6b7e33bd2cba8d4
Yao Jean-Claude <jeandev84@yahoo.com>
2019-12-04 13:49:31 +0300

    Liste des Regions

--- api/src/Controller/ApiController.php
+++ api/src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 /**
@@ -14,13 +15,19 @@ class ApiController extends AbstractController
 {
     /**
      * @Route("/listeRegions", name="listeRegions")
-    */
-    public function listeRegion()
+     * @param SerializerInterface $serializer [ jsonToArray, ArrayToJson, ObjectToJson ]
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function listeRegion(SerializerInterface $serializer)
     {
         $mesRegions = file_get_contents('https://geo.api.gouv.fr/regions');
-        dump($mesRegions);
-        die;
-        return $this->render('api/index.html.twig');
+        $mesRegionsTab = $serializer->decode($mesRegions, 'json'); // json to array
+
+        /* dd($mesRegionsTab); */
+
+        return $this->render('api/index.html.twig', [
+            'mesRegions' => $mesRegionsTab
+        ]);
     }
 
 
